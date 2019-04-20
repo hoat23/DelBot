57cf94e46610efc354d24de3f92775b2b09dbd26
Deiner Zapata <deinerzapata@gmail.com>
2019-04-20 00:30:38 -0500

    Add files via upload

--- chatbot/api_bible.py
+++ chatbot/api_bible.py
@@ -25,7 +25,7 @@ def get_versiculo(passage, biblia_version = "RVR60.js"):
         txt = rpt['text'].split("\r\n")
     else:
         txt = ["Upsss... algo anda mal."]
-    print("{0}|INFO | get_versiculo | {1}".format( datetime.utcnow().isoformat() , txt[0] ))
+        print("{0}|WARN | get_versiculo | {1}".format( datetime.utcnow().isoformat() , txt[0] ))
     return txt
 #######################################################################################
 def req_get(get_json , timeout=None):
@@ -34,8 +34,8 @@ def req_get(get_json , timeout=None):
         headers = {'Content-Type': 'application/json'}
         URL_API = get_json['uri']
         rpt = requests.get( url=URL_API , headers=headers, timeout=timeout)
-        #if not( (rpt.status_code)==200 or (rpt.status_code)==201 ):
-        print("{0}|INFO | req_get | {1} | {2} | URL_API=[{3}]".format( datetime.utcnow().isoformat() , rpt.status_code, rpt.reason, URL_API[:40]) )
+        if not( (rpt.status_code)==200 or (rpt.status_code)==201 ):
+            print("{0}|WARN | req_get | {1} | {2} | URL_API=[{3}]".format( datetime.utcnow().isoformat() , rpt.status_code, rpt.reason, URL_API[:40]) )
         json_rpt = rpt.json()#json_rpt = rpt.text
     except:
         print("{0}|ERROR| req_get ".format(datetime.utcnow().isoformat()) )
